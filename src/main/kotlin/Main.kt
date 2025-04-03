558798ff51a39960f6958ee2cafac109b984564b
wlt <1343539249@qq.com>
2025-04-03 11:01:57 +0800

    refactor(kotlin): 重构 MCP 服务器初始化流程

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,13 @@
-import io.ktor.server.application.Application
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.modelcontextprotocol.kotlin.sdk.*
+import io.modelcontextprotocol.kotlin.sdk.Implementation
+import io.modelcontextprotocol.kotlin.sdk.ServerCapabilities
 import io.modelcontextprotocol.kotlin.sdk.server.RegisteredTool
 import io.modelcontextprotocol.kotlin.sdk.server.Server
 import io.modelcontextprotocol.kotlin.sdk.server.ServerOptions
 import io.modelcontextprotocol.kotlin.sdk.server.mcp
-import org.koin.core.qualifier.named
 import org.koin.ktor.ext.getKoin
-import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.koin
 import tools.toolModule
 
@@ -21,25 +20,25 @@ fun main() {
 
 
 fun Application.mainModule() {
-    koin{
+    koin {
         modules(toolModule)
     }
 
-    val server = Server(
-        Implementation(
-            name = "private mcp toolkit server", // Tool name is "weather"
-            version = "1.0.0" // Version of the implementation
-        ),
-        ServerOptions(
-            capabilities = ServerCapabilities(tools = ServerCapabilities.Tools(listChanged = true))
-        )
-    )
-    /*get all tools by koin*/
-    val tools = getKoin().getAll<RegisteredTool>()
-    server.addTools(tools)
-
     /*register the mcp server*/
     mcp {
+        val server = Server(
+            Implementation(
+                name = "private mcp toolkit server", // Tool name is "weather"
+                version = "1.0.0" // Version of the implementation
+            ),
+            ServerOptions(
+                capabilities = ServerCapabilities(tools = ServerCapabilities.Tools(listChanged = true))
+            )
+        )
+        /*get all tools by koin*/
+        val tools = getKoin().getAll<RegisteredTool>()
+
+        server.addTools(tools)
         server
     }
 
